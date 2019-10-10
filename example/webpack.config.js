3b2fa3aa7c25558b1ef1441aef95e5d977efe237
GuDuJian-J-Zhang <zhangjun_dg@mail.dlut.edu.cn>
2019-10-10 17:50:37 +0800

    add loader for pug

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,4 +1,23 @@
 const path = require('path');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+
+let templates = [];
+let dir = './example';
+let files = fs.readdirSync(dir);
+
+files.forEach(file => {
+  if (file.match(/\.pug$/)) {
+    let filename = file.substring(0, file.length - 4);
+    templates.push(
+      new HtmlWebpackPlugin({
+        template: dir + '/' + filename + '.pug',
+        filename: filename + '.html'
+      })
+    );
+  }
+});
 
 module.exports = {
   entry: './scene_example.ts',
@@ -9,8 +28,19 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.pug$/,
+        use: [
+          "raw-loader",
+          "pug-html-loader"
+        ]
+      }
     ],
   },
+  plugins: [
+    ...templates,
+    new HtmlWebpackPugPlugin()
+  ],
   resolve: {
     extensions: [ '.ts'],
   },
